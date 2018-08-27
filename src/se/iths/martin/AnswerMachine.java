94df41552abb463160002ac5c71314e83baeef91
Martin Blomberg <martin.blomberg@outlook.com>
2018-08-27 09:20:02 +0200

    Inläsning av textsträngar som kommandon.

---
+++ src/se/iths/martin/AnswerMachine.java
@@ -0,0 +1,18 @@
+package se.iths.martin;
+
+import java.util.Scanner;
+
+public class AnswerMachine {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String someText = "Say something: ";
+
+        System.out.println(someText);
+
+        String input = sc.nextLine();
+
+
+    }
+}
