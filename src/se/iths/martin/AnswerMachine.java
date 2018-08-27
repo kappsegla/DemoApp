2fe2606c227147d3dd0c3b59f4563bbfc87baf5f
Martin Blomberg <martin.blomberg@outlook.com>
2018-08-27 13:41:38 +0200

    String parsing with substring and parseInt

--- src/se/iths/martin/AnswerMachine.java
+++ src/se/iths/martin/AnswerMachine.java
@@ -7,6 +7,32 @@ import java.util.Scanner;
 
 public class AnswerMachine {
 
+    //Method overloading
+    //Method name + number of parameters and parameter type is method signature
+    //Return type is not included
+
+    public static int addTwoNumbers(int numberOne, int numberTwo ) {
+        return numberOne + numberTwo;
+    }
+
+    public static int addTwoNumbers(double numberOne, double numberTwo ) {
+        return (int) numberOne + (int) numberTwo;
+    }
+
+    public static int addTwoNumbers(int numberOne) {
+        return numberOne + 10;
+    }
+
+    public static int doSomething() {
+        int i = 1;
+        i++;
+        return i;
+    }
+
+    public static void printYourName() {
+        System.out.println("My name is Maja, what's yours?");
+    }
+
     public static void printCurrentDate() {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
@@ -31,12 +57,34 @@ public class AnswerMachine {
                 //return;  //Avslutar metoden, blir samma resultat i det här fallet
             } else if (input.contains("date") || input.contains("datum")) {
                 printCurrentDate();
-            } else {
+            }else if (input.equals("!")){
+                int answer = addTwoNumbers(10.0, 10.0);
+            }
+            else if(input.startsWith("mul")){
+
+                //Parses a string with format "mul 4" into integer
+                int tabell = Integer.parseInt( input.substring(4) );
+
+                printMultiplicationTable(tabell);
+
+          /*      int i = 1;
+                while(i < 11){
+                    System.out.println( tabell + " * " + i + " = " + tabell * i);
+                    i++;
+                }
+                */
+            }
+            else {
                 System.out.println("What?");
             }
         }
     }
-    public static void printYourName() {
-        System.out.println("My name is Maja, what's yours?");
+
+    public static void printMultiplicationTable(int tabell) {
+        for( int i = 1; i < 11 ; i++ )
+        {
+            System.out.println( tabell + " * " + i + " = " + tabell * i);
+        }
     }
+
 }
