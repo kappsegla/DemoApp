ed578b81b520867d27c1db167bf540258da28064
Martin Blomberg <martin.blomberg@outlook.com>
2018-08-27 10:34:47 +0200

    Dagens datum

--- src/se/iths/martin/AnswerMachine.java
+++ src/se/iths/martin/AnswerMachine.java
@@ -1,5 +1,8 @@
 package se.iths.martin;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class AnswerMachine {
@@ -18,9 +21,14 @@ public class AnswerMachine {
             } else if (input.contains("Name")) {
                 System.out.println("My name is Maja, what's yours?");
             } else if (input.equals("hej då")) {
-                break;
-                //return;
-            } else {
+                break;  //Avbryter while loopen och fortsätter med koden efter loopen.
+                //return;  //Avslutar metoden, blir samma resultat i det här fallet
+            } else if(input.contains("date") || input.contains("datum") ) {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = new Date();
+                System.out.println(dateFormat.format(date));
+            }
+            else {
                 System.out.println("What?");
             }
         }
