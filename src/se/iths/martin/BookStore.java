d8f30a0a817556949c8bc37df9c41ead20500735
Martin Blomberg <martin.blomberg@outlook.com>
2018-09-07 11:00:49 +0200

    BooksStore, updated testcode for Book implementation

--- src/se/iths/martin/BookStore.java
+++ src/se/iths/martin/BookStore.java
@@ -7,13 +7,12 @@ public class BookStore {
     public static void main(String[] args) {
         ArrayList<Book> books = new ArrayList<>();
 
-        books.add(new Book("Astrid Lindgren", "Pippi Långstrump"));
+        books.add(new Book("Pippi Långstrump","Astrid Lindgren"));
         books.add(new Book("The Stand"));
-
-        System.out.println("Totalt antal böcker:" + Book.getNumberOfBooks());
 
         for (Book b : books) {
             System.out.println(b);
         }
+        System.out.println("Totalt antal böcker:" + Book.getNumberOfBooks());
     }
 }
