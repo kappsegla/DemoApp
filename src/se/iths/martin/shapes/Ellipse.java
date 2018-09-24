72f457c002ff2a9069b8152e9629a6ac66ec1a94
Martin Blomberg <martin.blomberg@outlook.com>
2018-09-24 10:40:17 +0200

    Ellipse

---
+++ src/se/iths/martin/shapes/Ellipse.java
@@ -0,0 +1,24 @@
+package se.iths.martin.shapes;
+
+public class Ellipse extends Shape {
+    public Ellipse(double length, double height) {
+        super(length, height);
+    }
+
+    public Ellipse(Shape shape) {
+        super(shape);
+    }
+
+    @Override
+    public double getArea() {
+        return (getLength() / 2.0) * (getHeight() / 2.0) * Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        double a = getLength();
+        double b = getHeight();
+        //https://sv.wikipedia.org/wiki/Ellips_(matematik)#Omkrets
+        return Math.PI * ( 3*(a+b) - Math.sqrt((3*a + b)*(a + 3*b)));
+    }
+}
